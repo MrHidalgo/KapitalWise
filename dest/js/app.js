2036c49fe9cc721e3d1ff1bd482b8f192a1ac5aa
Andrii <Luckyhidalgo@gmail.com>
2020-06-02 10:31:48 +0300

    Added main illustration with animation.

--- dest/js/app.js
+++ dest/js/app.js
@@ -26,7 +26,29 @@ var initPreventBehavior = function initPreventBehavior() {
 	/*
  * CALLBACK :: start
  * ============================================= */
+	var illustrationAnimation = function illustrationAnimation() {
+		var mainAnimation = function mainAnimation() {
+			var tl = new TimelineMax();
 
+			var _svg = $('#main-illustration-svg');
+
+			tl.set(_svg, { visibility: "visible" });
+			tl.set("#main-box--left", { opacity: 0, transformOrigin: 'center' });
+			tl.set("#main__box-line > *", { transformOrigin: 'left center', scaleX: 0 });
+			tl.set("#main__box-graph > *", { transformOrigin: 'center bottom', scaleY: '0' });
+			tl.set("#main__box-point > *", { transformOrigin: 'center', y: 20, scale: '0' });
+			tl.set("#main__box-point > *", { transformOrigin: 'center', y: 20, scale: '0' });
+			tl.set("#main__sidebar-point > *", { transformOrigin: 'center', scale: '0' });
+			tl.set("#main__sidebar-line > *", { transformOrigin: 'left center', scaleX: '0' });
+			tl.set("#main__content-line-1 > *", { transformOrigin: 'left center', scaleX: '0' });
+			tl.set("#main__content-line-2 > *", { transformOrigin: 'left center', scaleX: '0' });
+			tl.set("#main__content-line-3 > *", { transformOrigin: 'left center', scaleX: '0' });
+
+			tl.to($('#main-box--left'), 0.4, { opacity: 1, ease: Power1.easeInOut }).staggerTo($('#main__box-line > *'), 0.5, { scaleX: 1, ease: Power1.easeInOut }, 0.075, '-=0.2').staggerTo($('#main__box-graph > *'), 0.5, { scaleY: 1, ease: Power1.easeInOut }, 0.075, '-=0.3').staggerTo($('#main__box-point > *'), 0.5, { scale: 1, y: 0, ease: Power1.easeInOut }, 0.075, '-=0.3').staggerTo($('#main__sidebar-point > *'), 0.55, { scale: 1, ease: Power1.easeInOut }, 0.1, '-=1').staggerTo($('#main__sidebar-line > *'), 0.55, { scaleX: 1, ease: Power1.easeInOut }, 0.1, '-=1').staggerTo($('#main__content-line-1 > *'), 0.5, { scaleX: 1, ease: Power1.easeInOut }, 0.075, '-=0.75').staggerTo($('#main__content-line-2 > *'), 0.5, { scaleX: 1, ease: Power1.easeInOut }, 0.075, '-=0.75').staggerTo($('#main__content-line-3 > *'), 0.5, { scaleX: 1, ease: Power1.easeInOut }, 0.075, '-=0.75');
+		};
+
+		mainAnimation();
+	};
 	/*
  * CALLBACK :: end
  * ============================================= */
@@ -45,6 +67,7 @@ var initPreventBehavior = function initPreventBehavior() {
 		// ==========================================
 
 		// callback
+		illustrationAnimation();
 		// ==========================================
 	};
 	initNative();
--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -5,7 +5,39 @@
 	/*
 	* CALLBACK :: start
 	* ============================================= */
+	const illustrationAnimation = () => {
+		const mainAnimation = () => {
+			const tl = new TimelineMax();
 
+			const _svg = $('#main-illustration-svg');
+
+			tl.set(_svg, {visibility: "visible"});
+			tl.set("#main-box--left", {opacity: 0, transformOrigin: 'center'});
+			tl.set("#main__box-line > *", {transformOrigin: 'left center', scaleX: 0,});
+			tl.set("#main__box-graph > *", {transformOrigin: 'center bottom', scaleY: '0',});
+			tl.set("#main__box-point > *", {transformOrigin: 'center', y: 20, scale: '0',});
+			tl.set("#main__box-point > *", {transformOrigin: 'center', y: 20, scale: '0',});
+			tl.set("#main__sidebar-point > *", {transformOrigin: 'center', scale: '0',});
+			tl.set("#main__sidebar-line > *", {transformOrigin: 'left center', scaleX: '0',});
+			tl.set("#main__content-line-1 > *", {transformOrigin: 'left center', scaleX: '0',});
+			tl.set("#main__content-line-2 > *", {transformOrigin: 'left center', scaleX: '0',});
+			tl.set("#main__content-line-3 > *", {transformOrigin: 'left center', scaleX: '0',});
+
+			tl
+				.to($('#main-box--left'), 0.4, {opacity: 1, ease: Power1.easeInOut})
+				.staggerTo($('#main__box-line > *'), 0.5, {scaleX: 1, ease: Power1.easeInOut}, 0.075, '-=0.2')
+				.staggerTo($('#main__box-graph > *'), 0.5, {scaleY: 1, ease: Power1.easeInOut}, 0.075, '-=0.3')
+				.staggerTo($('#main__box-point > *'), 0.5, {scale: 1, y: 0, ease: Power1.easeInOut}, 0.075, '-=0.3')
+				.staggerTo($('#main__sidebar-point > *'), 0.55, {scale: 1, ease: Power1.easeInOut}, 0.1, '-=1')
+				.staggerTo($('#main__sidebar-line > *'), 0.55, {scaleX: 1, ease: Power1.easeInOut}, 0.1, '-=1')
+				.staggerTo($('#main__content-line-1 > *'), 0.5, {scaleX: 1, ease: Power1.easeInOut}, 0.075, '-=0.75')
+				.staggerTo($('#main__content-line-2 > *'), 0.5, {scaleX: 1, ease: Power1.easeInOut}, 0.075, '-=0.75')
+				.staggerTo($('#main__content-line-3 > *'), 0.5, {scaleX: 1, ease: Power1.easeInOut}, 0.075, '-=0.75')
+			;
+		};
+
+		mainAnimation();
+	};
 	/*
 	* CALLBACK :: end
 	* ============================================= */
@@ -25,6 +57,7 @@
 		// ==========================================
 
 		// callback
+		illustrationAnimation();
 		// ==========================================
 	};
 	initNative();
